1bdd92c149283540b8d6a4b5965e72fae2bb432e
Shivam Lakhani <shivamlakhani03@gmail.com>
2025-06-20 00:17:50 +0100

    pre-asteroid implementation commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import pygame
 
 from constants import *
-
+from player import Player
 
 
 
@@ -15,11 +15,31 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+
+
+
+    clock = pygame.time.Clock()
+    dt=0
+    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
     while True:
         for event in pygame.event.get():
+            
             if event.type == pygame.QUIT:
                 return
-    screen.fill('black')
-    screen.flip()
+                
+        dt=clock.tick(60)/1000
+            
+        screen.fill('black')
+        
+        
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+    
 if __name__ == "__main__":
     main()
